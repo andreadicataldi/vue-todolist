c87e3b9a0e4ae461b0ee4d88954018177b6263d9
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2020-12-16 13:17:13 +0100

    add Todo working

---
+++ main.js
@@ -0,0 +1,17 @@
+let app = new Vue({
+  el: "#app",
+  data: {
+    message: "",
+    list: [],
+  },
+  methods: {
+    addTodo() {
+      if (this.message.length >= 4) {
+        this.list.push(this.message);
+      }
+    },
+    removeTodo() {
+      this.list.splice(this.message);
+    },
+  },
+});
